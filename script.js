1b10acf81ff8a4e216307be65e26facbfe8ddeab
TheTanishqChauhan <155085790+thetanishqchauhan@users.noreply.github.com>
2024-05-27 23:01:03 +0530

    Update Resume

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 // auto downloading my resume
 function downloadDocument() {
 
-    var documentUrl = 'Resume-Tanishq-Chauhan.pdf';
+    var documentUrl = 'Tanishq Chauhan Resume - MIET.pdf';
     var downloadLink = document.createElement('a');
     downloadLink.href = documentUrl;
-    downloadLink.download = 'Resume-Tanishq-Chauhan.pdf';
+    downloadLink.download = 'Tanishq Chauhan Resume - MIET.pdf';
     document.body.appendChild(downloadLink);
     downloadLink.click();
     document.body.removeChild(downloadLink);
